cbbe9442ee3f4b09bedb70981b5dcc3c2ae1a356
hussaa54 <hussaa54@mcmaster.ca>
2021-01-21 22:02:15 -0500

    area, is_valid fxns created

--- A1/src/triangle_adt.py
+++ A1/src/triangle_adt.py
@@ -23,5 +23,15 @@ class TriangleT:
 		return lstself == lsttri
 
 	def perim(self):
-		return self.__a + self.__b + self.__c
+		return int(self.__a + self.__b + self.__c)
 
+	def area(self):
+		s = (self.__a + self.__b + self.__c)/2
+		A = sqrt(s*(s-self.__a)*(s-self.__b)*(s-self.__c))
+		return float(A)
+
+	def is_valid(self):
+		if self.__a + self.__b <= self.__c or self.__a + self.__c <= self.__b or self.__b + self.__c <= self.__a:
+			return False
+		else:
+			return True
