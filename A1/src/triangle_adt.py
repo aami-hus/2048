1859b9934ac6eb06f6d1cf88cac289c63a673163
hussaa54 <hussaa54@mcmaster.ca>
2021-01-21 21:39:21 -0500

    constructor, getter, equal, perim fxns completed

--- A1/src/triangle_adt.py
+++ A1/src/triangle_adt.py
@@ -3,3 +3,25 @@
 #  @brief 
 #  @date 
 
+from math import sqrt, atan, pi
+
+class TriangleT:
+
+	def __init__(self, a, b, c):
+		self.__a = int(a)
+		self.__b = int(b)
+		self.__c = int(c)
+
+	def get_sides(self):
+		return (self.__a, self.__b, self.__c)
+
+	def equal(self, triangle):
+		lstself = [self.__a, self.__b, self.__c]
+		lsttri = list(triangle.get_sides())
+		lstself.sort()
+		lsttri.sort()
+		return lstself == lsttri
+
+	def perim(self):
+		return self.__a + self.__b + self.__c
+
