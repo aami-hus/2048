0952fb5ef56000b114b135cfc2dde653afca2e9b
hussaa54 <hussaa54@mcmaster.ca>
2021-01-21 19:30:11 -0500

    completed constructor, getters, and equal fxn

--- A1/src/complex_adt.py
+++ A1/src/complex_adt.py
@@ -3,3 +3,34 @@
 #  @brief 
 #  @date 
 
+from math import sqrt, atan, pi
+
+class ComplexT:
+
+	def __init__(self, x, y):
+		self.__x = x
+		self.__y = y
+
+	def real(self):
+		return self.__x
+
+	def imag(self):
+		return self.__y
+
+	def get_r(self):
+		return sqrt(self.__x**2 + self.__y**2)
+
+	def get_phi(self):
+		if x > 0 or y != 0:
+			return 2*atan(y/(self.get_r+x))
+		elif x < 0 and y == 0:
+			return pi
+		else:
+			return "The phase is undefined."
+
+	def equal(self, complexnum):
+		if self.__x == complexnum.real and self.__y == complexnum.imag:
+			return True
+		else:
+			return False
+
