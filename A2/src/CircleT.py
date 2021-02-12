e9b86f937d9f1b8f6eabc2dbbc844b1b82ada4c5
hussaa54 <hussaa54@mcmaster.ca>
2021-02-12 18:14:28 -0500

    CircleT constructor and access routines added

--- A2/src/CircleT.py
+++ A2/src/CircleT.py
@@ -2,3 +2,26 @@
 #  @author
 #  @brief
 #  @date
+from Shape import Shape
+
+class CircleT:
+
+	def __init__(self, xs, ys, rs, ms):
+		if not(rs > 0 and ms > 0):
+			raise ValueError
+		self.x = xs
+		self.y = ys
+		self.r = rs
+		self.m = ms
+		
+	def cm_x(self):
+		return self.x
+
+	def cm_y(self):
+		return self.y
+
+	def mass(self):
+		return self.m
+
+	def m_inert(self):
+		return (self.m*(self.r**2))/2	
