9aec434b4cf45f49bc6ad302f2e98c3e602dffb3
hussaa54 <hussaa54@mcmaster.ca>
2021-02-16 12:33:02 -0500

    fixing git issues; re-committing files

--- A2/src/Plot.py
+++ A2/src/Plot.py
@@ -3,3 +3,16 @@
 #  @brief
 #  @date
 #  @details
+
+import matplotlib.pyplot as plt
+
+def plot(w, t):
+	if not(len(w)==len(t)):
+		return ValueError
+	x = [w[i][0] for i in range(len(w))]
+	y = [w[i][1] for i in range(len(w))]
+	fig, axs = plt.subplots(3)
+	fig.suptitle('Motion Simulation')
+	axs[0].plot(x, t)
+	axs[1].plot(y, t)
+	axs[2].plot(y, x)
