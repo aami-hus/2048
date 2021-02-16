28be4c120cf51de803399fc487a635ab3243dfd4
hussaa54 <hussaa54@mcmaster.ca>
2021-02-16 15:04:39 -0500

    converted tabs to spaces, fixed flake8 errors

--- A2/src/TriangleT.py
+++ A2/src/TriangleT.py
@@ -5,24 +5,25 @@
 
 from Shape import Shape
 
+
 class TriangleT:
 
-	def __init__(self, x, y, s, m):
-		if not(s > 0 and m > 0):
-			return ValueError
-		self.x = x
-		self.y = y
-		self.s = s
-		self.m = m
+    def __init__(self, x, y, s, m):
+        if not(s > 0 and m > 0):
+            return ValueError
+        self.x = x
+        self.y = y
+        self.s = s
+        self.m = m
 
-	def cm_x(self):
-		return self.x
+    def cm_x(self):
+        return self.x
 
-	def cm_y(self):
-		return self.y
+    def cm_y(self):
+        return self.y
 
-	def mass(self):
-		return self.m
+    def mass(self):
+        return self.m
 
-	def m_inert(self):
-		return (self.m*(self.s**2))/12
+    def m_inert(self):
+        return (self.m * (self.s**2)) / 12
