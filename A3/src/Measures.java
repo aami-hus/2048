d2e681a582b7cd11f4a7d117406ef33038f453f2
hussaa54 <hussaa54@mcmaster.ca>
2021-03-26 14:27:59 -0400

    implemented measures interface

--- A3/src/Measures.java
+++ A3/src/Measures.java
@@ -1,1 +1,15 @@
 package src;
+
+import src.IndicatorT;
+import src.AttributeT;
+import java.util.*;
+
+public interface Measures{
+
+	public double[] measures() throws UnsupportedOperationException;
+
+	public double[] measures(IndicatorT ind) throws UnsupportedOperationException;
+
+	public double[] measures(AttributeT att) throws UnsupportedOperationException;
+
+}
