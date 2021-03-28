8c1c1d4f0ff062ce526c2d305960886c55a021a1
hussaa54 <hussaa54@mcmaster.ca>
2021-03-27 20:55:48 -0400

    added public static method norm

--- A3/src/Services.java
+++ A3/src/Services.java
@@ -1,1 +1,19 @@
 package src;
+
+public class Services{
+
+	public static double[] normal(double[] v){
+
+		double total = 0;
+		double [] normArr = new double[v.length]; 
+		for(double i : v){
+			total = total + i;
+		}
+		for(int i = 0; i < v.length; i++){
+			normArr[i] = v[i]/total;
+		}
+		return normArr;
+
+	}
+
+}
