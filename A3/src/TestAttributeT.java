69ff3a8f6ea1fe8873c6c63e823152ae011ad244
hussaa54 <hussaa54@mcmaster.ca>
2021-03-29 20:29:09 -0400

    finished test cases for AttributeT class

--- A3/src/TestAttributeT.java
+++ A3/src/TestAttributeT.java
@@ -1,22 +1,64 @@
 /**
- * Author: 
- * Revised: 
+ * Author: Aamina Hussain
+ * Revised: March 29, 2021
  * 
- * Description: 
+ * Description: Testing AttributeT class
  */
 
 package src;
 
 import org.junit.*;
 import static org.junit.Assert.*;
+import src.IndicatorT;
+import src.AttributeT;
+import java.util.*;
 
 public class TestAttributeT
 {
 
-    @Test
-    public void testBlank()
+	private AttributeT Design;
+	private AttributeT TestEmpty;
+
+	@Before
+    public void setUp()
     {
-        assertTrue(true);
+        Design = new AttributeT("Design", new IndicatorT[] {IndicatorT.desProcess, IndicatorT.desProcess, IndicatorT.standards});
+        TestEmpty = new AttributeT("", new IndicatorT[]{});
+    }
+
+    @After
+    public void tearDown()
+    {
+        Design = null;
+        TestEmpty = null;
+    }
+
+    @Test
+    public void testgetName()
+    {
+        assertTrue(Design.getName() == "Design");
+    }
+
+    @Test
+    public void testgetNameEdgeCase()
+    {
+        assertTrue(TestEmpty.getName() == "");
+    }
+
+    @Test
+    public void testgetIndicators()
+    {
+    	boolean a = false;
+    	if(Arrays.asList(Design.getIndicators()).contains(IndicatorT.desProcess) && Arrays.asList(Design.getIndicators()).contains(IndicatorT.standards)){
+    		a = true;
+    	}
+        assertTrue(a);
+    }
+
+    @Test
+    public void testgetIndicatorsEdgeCase()
+    {
+        assertTrue(TestEmpty.getIndicators().length == 0);
     }
 
 }
