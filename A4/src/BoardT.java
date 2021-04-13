e9fe790fe414ff00dd0dc8d47c09d4bfaf1b6354
hussaa54 <hussaa54@mcmaster.ca>
2021-04-13 18:09:42 -0400

    added doxygen comments

--- A4/src/BoardT.java
+++ A4/src/BoardT.java
@@ -1,23 +1,35 @@
 /**
- * Author: Aamina Hussain, hussaa54
- * Revised: April 12, 2021
+ * Author: Aamina Hussain
+ * Revised: April 13, 2021
+ *
+ * Description: A model module for storing the state and status of the game 2048
  */
 
 package src;
 
-import src.MoveT;
+// Import java libraries
 import java.util.*;
 
+/**
+ * @brief An ADT that represents a board of the game 2048
+ * @details The board is represented as a 2D integer array and keeps track of the score
+ */
 public class BoardT{
 	
+	// State Variables
 	private int[][] board;
 	private int score;
 	private ArrayList<ArrayList<Integer>> empty;
 	private boolean win;
 	private boolean lose;
 
+	// Exported Constants
 	public final static int size = 4;
 
+   /**
+	* @brief Initializes a board object
+	* @details creates a board with two 2's or one 2 and one 4 in random cells
+	*/
 	public BoardT(){
 		this.board = new int[4][4];
 		this.score = 0;
@@ -65,43 +77,82 @@ public class BoardT{
 		}
 	}
 
+   /**
+	* @brief Gets the current state of the board
+	* @return The current state of the board
+	*/
 	public int[][] getBoard(){
 		return this.board;
 	}
 
+   /**
+	* @brief Gets the current score
+	* @return The current score
+	*/
 	public int getScore(){
 		return this.score;
 	}
 
+   /**
+	* @brief Gets the the set of empty cells that are on the board
+	* @return The set of empty cells
+	*/
 	public ArrayList<ArrayList<Integer>> emptyCells(){
 		return this.empty;
 	}
 
+   /**
+	* @brief Gets whether the game has been won or not
+	* @return True if the game has been won
+	*/
 	public boolean isGameWon(){
 		return this.win;
 	}
 
+   /**
+	* @brief Gets whether the game has been lost or not
+	* @return True if the game has been lost
+	*/
 	public boolean isGameLost(){
 		return this.lose;
 	}
 
 	// assumption: input will always be 4x4 array
 	// assumption: all numbers in array will be correct (aka 0 or a power of 2 (not including 1))
+
+   /**
+	* @brief Sets the board to a predefined board
+	* @details Makes it easier for testing purposes since values are not random
+	* @param b a board that has already been created
+	*/
 	public void setBoard(int[][] b){
 		this.board = b;
 	}
 
+   /**
+	* @brief Generates a random number between (and including) two given numbers
+	* @param min The lower bound of the range
+	* @param max The upper bound of the range
+	* @return The randomly generated number
+	*/
 	public int randomGenerator(int min, int max){
 		int randomNum = (int)Math.floor(Math.random()*(max-min+1)+min);
 		return randomNum;
 	}
 
+   /**
+	* @brief Restarts/reinitializes the current game
+	*/
 	public void resetBoard(){
 		BoardT temp = new BoardT();
 		this.board = temp.board;
 		temp = null;
 	}
 
+   /**
+	* @brief Checks if the player can make a move right
+	* @return True if the player can move right
+	*/
 	public boolean isValidMoveRight(){
 		for(int i = 0; i < this.size; i++){
 			if(this.board[i][0] == 0 && this.board[i][1] == 0 && this.board[i][2] == 0){
@@ -128,6 +179,10 @@ public class BoardT{
 		return false;
 	}
 
+   /**
+	* @brief Checks if the player can make a move left
+	* @return True if the player can move left
+	*/
 	public boolean isValidMoveLeft(){
 		for(int i = 0; i < this.size; i++){
 			if(this.board[i][3] == 0 && this.board[i][2] == 0 && this.board[i][1] == 0){
@@ -154,6 +209,10 @@ public class BoardT{
 		return false;
 	}
 
+   /**
+	* @brief Checks if the player can make a move up
+	* @return True if the player can move up
+	*/
 	public boolean isValidMoveUp(){
 		for(int i = 0; i < this.size; i++){
 
@@ -177,6 +236,10 @@ public class BoardT{
 		return false;
 	}
 
+   /**
+	* @brief Checks if the player can make a move down
+	* @return True if the player can move down
+	*/
 	public boolean isValidMoveDown(){
 		for(int i = 0; i < this.size; i++){
 			
@@ -200,6 +263,10 @@ public class BoardT{
 		return false;
 	}
 
+   /**
+	* @brief Executes the move to the right
+	* @details Shifts tiles over to the right, combines tiles with the same value, updates the score, and checks if the player won or lost.
+	*/
 	public void moveRight(){
 
 		if(!(isValidMoveRight())){
@@ -258,12 +325,15 @@ public class BoardT{
 		this.board[this.empty.get(randomCell).get(0)][this.empty.get(randomCell).get(1)] = 2;
 		this.empty.remove(randomCell);
 
-		// call fxn to display "try again" or something
 		if(!(this.isValidMoveRight() && this.isValidMoveLeft() && this.isValidMoveUp() && this.isValidMoveDown())){
 			this.lose = true;
 		}
 	}
 
+   /**
+	* @brief Executes the move to the left
+	* @details Shifts tiles over to the left, combines tiles with the same value, updates the score, and checks if the player won or lost.
+	*/
 	public void moveLeft(){
 
 		if(!(isValidMoveLeft())){
@@ -328,6 +398,10 @@ public class BoardT{
 		}
 	}
 
+   /**
+	* @brief Executes the move up
+	* @details Shifts tiles up, combines tiles with the same value, updates the score, and checks if the player won or lost.
+	*/
 	public void moveUp(){
 
 		if(!(isValidMoveUp())){
@@ -392,6 +466,10 @@ public class BoardT{
 		}
 	}
 
+   /**
+	* @brief Executes the move down
+	* @details Shifts tiles down, combines tiles with the same value, updates the score, and checks if the player won or lost.
+	*/
 	public void moveDown(){
 
 		if(!(isValidMoveRight())){
